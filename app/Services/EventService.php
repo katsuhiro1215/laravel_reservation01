0d71453547e3e3c9effd1171d7c57710f86b460d
katsuhiro.k1215@gmail.com <katsuhiro.k1215@gmail.com>
2023-03-09 01:21:15 +0900

    calendar

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -36,4 +36,20 @@ class EventService
 
     return $dateTime;
   }
+
+  public static function getWeekEvents($startDate, $endDate)
+  {
+    $reservedPeople = DB::table('reservations')
+      ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+      ->whereNull('canceled_date')
+      ->groupBy('event_id');
+
+    return DB::table('events')
+      ->leftJoinSub($reservedPeople, 'reservedPeople', function ($join) {
+        $join->on('events.id', '=', 'reservedPeople.event_id');
+      })
+      ->whereBetween('events.start_date', [$startDate, $endDate])
+      ->orderBy('events.start_date', 'asc')
+      ->get();
+  }
 }
--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,14 +16,29 @@ class EventFactory extends Factory
      */
     public function definition()
     {
+
+        $avaiableHour = $this->faker->numberBetween(8, 20);
+        $minutes = [0, 30];
+        $mKey = array_rand($minutes);
+        $addHour = $this->faker->numberBetween(1, 3);
+
         $dummyDate = $this->faker->dateTimeThisMonth;
-        
+        $startDate = $dummyDate->setTime($avaiableHour, $minutes[$mKey]);
+
+        $clone = clone $startDate;
+
+        $endDate = $clone->modify('+' . $addHour . 'hour');
+
+        // dd($startDate, $endDate);
+
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1, 20),
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            // 'start_date' => $dummyDate->format('Y-m-d H:i:s'),
+            'start_date' =>  $startDate,
+            // 'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean,
         ];
     }
